cf0fb0af71a0732cf45a5f6813dfa1bf1fa0d076
robarca10 <rohanpg2004@gmail.com>
2024-04-03 00:04:34 +0530

    Refactor code to use async/await and handle errors

--- index.js
+++ index.js
@@ -17,32 +17,47 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-app.get("/", async (req, res) => {
+async function checkVisited(){
   const result= await db.query("SELECT country_code FROM visited_countries");
   // console.log(result.rows);
   let countries = [];
   result.rows.forEach( (country)=>{
     countries.push(country.country_code);
   });
+  return countries;
+
+}
+
+app.get("/", async (req, res) => {
+  const countries= await checkVisited();
+  // console.log(result.rows);
    res.render("index.ejs", {countries: countries, total: countries.length})
 });
 
 app.post("/add", async (req, res)=>{
     const country= req.body["country"];
-    const result = await db.query(
-      "SELECT country_code FROM countries WHERE country_name = $1",
-      [country]
-    );
-    if(result.rows.length!=0){
-      const data= result.rows[0];
-      const countryCode= data.country_code;
-      db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [countryCode]);
-      res.redirect("/");
-    }else{
-      res.redirect("/",);
+    try{
+      const result = await db.query(
+        "SELECT country_code FROM countries WHERE country_name = $1",
+        [country]);
+        const data= result.rows[0];
+        const countryCode= data.country_code;
+        try{
+          const result= await db.query(
+            "SELECT * FROM visited_countries WHERE country_code = $1",
+            [countryCode])
+          } catch{
+            const countries= await checkVisited();
+            res.render("index.ejs", {countries: countries, total: countries.length, error: "Country already added!"}) 
+          };
+        }catch{
+          const countries= await checkVisited();
+          res.render("index.ejs", {countries: countries, total: countries.length, error: "Country not found!"})
+        }
     }
-})
+    
+
+)
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
