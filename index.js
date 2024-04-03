f7902de77dd040f0822589a726de41e265aaa2bd
robarca10 <rohanpg2004@gmail.com>
2024-04-03 11:21:47 +0530

    Refactor country search functionality and add remove feature

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function checkVisited(){
 
 app.get("/", async (req, res) => {
   const countries= await checkVisited();
-  // console.log(result.rows);
+  console.log(countries);
    res.render("index.ejs", {countries: countries, total: countries.length})
 });
 
@@ -38,26 +38,54 @@ app.post("/add", async (req, res)=>{
     const country= req.body["country"];
     try{
       const result = await db.query(
-        "SELECT country_code FROM countries WHERE country_name = $1",
-        [country]);
+        "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+        [country.toLowerCase()]);
         const data= result.rows[0];
         const countryCode= data.country_code;
+        
         try{
           const vcountries= await db.query(
             "SELECT * FROM visited_countries WHERE country_code = $1",
-            [countryCode])
-          } catch{
+            [countryCode]);            
+          await db.query("Insert into visited_countries(country_code) values($1)",[countryCode]);
+          console.log("Country added!");
+          res.redirect("/");
+            
+          } catch(err){
+            // console.log(err);
             const countries= await checkVisited();
             res.render("index.ejs", {countries: countries, total: countries.length, error: "Country already added!"}) 
           };
-        }catch{
+        }catch(err){
+          // console.log(err);
           const countries= await checkVisited();
           res.render("index.ejs", {countries: countries, total: countries.length, error: "Country not found!"})
         }
-    }
-    
+    });
 
-)
+app.post("/remove",async (req,res)=>{
+  const country = req.body["country"];
+  try{
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [country.toLowerCase()]);
+      const data= result.rows[0];
+      const countryCode= data.country_code;
+
+      try{
+        db.query("Delete from visited_countries where country_code=$1",[countryCode]);
+        res.redirect("/");
+      }catch(err){
+        // console.log(err);
+        const countries= await checkVisited();
+        res.render("index.ejs", {countries: countries, total: countries.length, error: "Country not highlighted"})
+      }
+  }catch(err){
+    // console.log(err);
+    const countries= await checkVisited();
+    res.render("index.ejs", {countries: countries, total: countries.length, error: "Country not found!"})
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
